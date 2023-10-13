493a1fa96db48326601675555c6722adabdc6b21
dtrinhh <dustintrinhh@gmail.com>
2023-10-12 23:35:51 -0600

    Base wireframe of Chamber

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,12 @@
+let lastmodified = document.lastModified;
+document.getElementById('lastmodified').innerHTML= lastmodified;
+
+// const options = {month: "long", day: "numeric", year: "numeric"};
+// document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+
+// const todaysdate = new Date();
+
+// document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// alert(document.lastModified);
+
