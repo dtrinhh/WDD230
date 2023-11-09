33747dbebcb84c08d7be346990899a9e09e10840
dtrinhh <dustintrinhh@gmail.com>
2023-11-09 12:17:32 -0700

    change to if statement

--- chamber/scripts/greeting.js
+++ chamber/scripts/greeting.js
@@ -4,11 +4,11 @@ let lastvisited = window.localStorage.getItem("lastvisited-ls");
 
 if (lastvisited < todaysdate) {
     // figuring out how to add variable in string
-    greeting.textContent = "You last visited days ago";
+    greeting.textContent = "Welcome! Let us know if you have any questions.";
 } else if (lastvisited = todaysdate) {
     greeting.textContent = "Back so soon! Awesome!";
 } else {
-    greeting.textContent = "Welcome! Let us know if you have any questions.";
+    greeting.textContent = "You last visited days ago";
 }
 
 localStorage.setItem("lastvisited-ls", lastvisited);
