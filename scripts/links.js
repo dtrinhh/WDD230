2fe31c420b3ac2a283c08e6dca11b32667b1467c
dtrinhh <dustintrinhh@gmail.com>
2023-11-10 23:08:29 -0700

    W09 JSON Data Assign.

--- scripts/links.js
+++ scripts/links.js
@@ -4,24 +4,53 @@ const linksURL = "https://dtrinhh.github.io/wdd230/data/links.json";
 async function getlinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-}
-getlinks();
    
-//     populatelesson(lessons);
-//     populatelinks(lessons);
-    
-// function populatelesson(obj) {
-//     const lessons = document.querySelector(".card1");
-//     const myh3 = document.createElement("h3");
-//     myh3.textContent = "Learning Activities";
-    
-// }
+    populatelesson(data);
+    populatelinks(data);
+}
 
-// function populatelinks(obj) {
-//     const unorderedli = document.querySelector("ul");
-//     const orderedli = document.createElement("li");
-//     orderedli.textContent = obj.lesson;
-//     unorderedli.appendChild(orderedli);
-//     orderedli.textContent = obj.links;} 
-// }
+function populatelesson() {
+    const card1 = document.querySelector(".card1");
+    const myh3 = document.createElement("h3");
+    myh3.textContent = "Learning Activities";
+    card1.appendChild(myh3);
+}
+
+function populatelinks(obj) {
+    const card1 = document.querySelector(".card1");
+    const lessons = obj.lessons;
+    const unorderedli = document.createElement("ul");
+
+    for (const lesson of lessons) {
+        const orderedli = document.createElement("li");
+        // const alinks = document.createElement("a");
+
+        orderedli.textContent = `Week ${lesson.lesson}: `;
+
+        const links = lesson.links;
+        for (const link of links) {
+            const alinks = document.createElement("a");
+            alinks.textContent = `${link.title}`;
+            alinks.href = link.url;
+            orderedli.append(alinks);
+            const space = " | ";
+            orderedli.append(space);
+        }
+
+        card1.appendChild(unorderedli);
+        unorderedli.appendChild(orderedli);
+        
+        
+
+        // unorderedli.textContent = link.lesson;
+        // activities.appendChild(unorderedli); 
+        // unorderedli.textContent = link.title;
+        // activities.appendChild(unorderedli); 
+    }
+}
+    // activities.appendChild(unorderedli); 
+    
+    // orderedli.textContent = links.title;
+    // unorderedli.appendChild(orderedli);
+
+getlinks();
