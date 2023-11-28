7f5724e69ef38f0212b1f731f8cf56872eafdb7c
dtrinhh <dustintrinhh@gmail.com>
2023-11-28 00:05:08 -0700

    deleted comment

--- chamber/scripts/hidden.js
+++ chamber/scripts/hidden.js
@@ -7,6 +7,3 @@ document.querySelector("submitBtn").addEventListener("submit", function(event) {
     const event= document.getElementById("timeStamp");
     event.value = getTimestamp();
 });
-
-console.log(getTimestamp());
-console.log(timestamp);
